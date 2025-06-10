fc07cd4edad20d9cfa26a83063a3c5710ee87519
fundish <funderchristian@gmail.com>
2025-06-10 21:19:13 +0200

    update

--- helperly-app/next.config.ts
+++ helperly-app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     NEXTAUTH_SECRET: "ZCdygiU3cl5dpPn7Jjmii6lk2uiVbenJQ922aW0nP0A=",
-    NEXT_PUBLIC_NEST_SERVICE: "http://localhost:3000",
+    NEXT_PUBLIC_NEST_SERVICE: "https://backend-wecx.onrender.com",
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
